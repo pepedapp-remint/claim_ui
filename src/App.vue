d9e41c05097731f0dc2e0936f3dad8a1e6b2c8fa
chef-pepe <ap334@disroot.org>
2020-11-19 16:24:48 -0800

    rename helloworld -> claimer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Claimer msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Claimer from './components/Claimer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Claimer
   }
 }
 </script>
--- src/components/Claimer.vue
+++ src/components/Claimer.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Claimer',
   props: {
     msg: String
   },
